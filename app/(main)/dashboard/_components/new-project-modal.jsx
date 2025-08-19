fcddd90360f4509db7004db9b1bc7ac787bd12b0
Abhishekfc <abhisheksir6280@gmail.com>
2025-08-20 02:18:55 +0530

    worked on project modal

--- app/(main)/dashboard/_components/new-project-modal.jsx
+++ app/(main)/dashboard/_components/new-project-modal.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { Badge } from '@/components/ui/badge';
 import { usePlanAccess } from '@/hooks/use-plan-access';
+import { api } from '@/convex/_generated/api';
+import { useConvexMutation, useConvexQuery } from '@/hooks/use-convex-query';
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { Button } from '@/components/ui/button';
 
 const NewProjectModal = ({ isOpen, onClose }) => {
+    const [isUploading, setIsUploading] = useState(false);
+    const [projectTitle, setProjectTitle] = useState("");
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [previewUrl, setPreviewUrl] = useState(null);
+
     const handleClose = () => {
         onClose();
     }
 
-    const {isFree, currentProjectCount} = usePlanAccess();
+    const { isFree, canCreateProject } = usePlanAccess();
+    const { data: projects } = useConvexQuery(api.projects.getUserProjects);
+    const { mutate: createProject } = useConvexMutation(api.projects.create);
+
+    const currentProjectCount = projects?.length || 0
+    const canCreate = canCreateProject(currentProjectCount)
+    
+    const handleCreateProject = () => {};
+
 
 
     return (
@@ -22,14 +39,70 @@ const NewProjectModal = ({ isOpen, onClose }) => {
 
                         {isFree && (
                             <Badge variant="secondry" className="bg-slate-700 text-white/70" >
-                                {currentProjectCount/3} projects
+                                {currentProjectCount}/3 projects
                             </Badge>
                         )}
 
 
                     </DialogHeader>
 
-                    <DialogFooter>Footer</DialogFooter>
+
+                    {isFree && currentProjectCount >= 2 && (
+
+
+                        <div className='space-y-6'>
+                            <Alert className="bg-amber-500/10 border-amber-500/20" >
+                                <Crown className="h-5 w-5 text-amber-400" />
+
+                                <AlertDescription className="text-amber-300/80">
+                                    <div className='font-semibold text-amber-400 mb-1'>
+                                        {currentProjectCount === 2
+                                            ? "last Free Project"
+                                            : "Projwct limit reached"
+                                        }
+
+                                        {currentProjectCount === 2
+                                            ? "This will be your last free Project. Upgrade to Pexel Pro for unlimited projects."
+                                            : "Free plan is limited to 3 projects. upgrade to Pexel Pro to create more projects."
+                                        }
+
+                                        {/* Upload Area */}
+
+                                    </div>
+                                </AlertDescription>
+                            </Alert>
+                        </div>
+
+                    )}
+
+
+                    <DialogFooter>
+                        <Button
+                            variant="ghost"
+                            onClick={handleClose}
+                            disabled={isUploading}
+                            className="text-white/70 hover:text-white">
+                            Cancel
+                        </Button>
+
+                        <Button
+                            onClick={handleCreateProject}
+                            disabled={!selectedFile || !projectTitle.trim() || isUploading}
+                            variant="primary">
+
+                                {isUploading  ? (
+                                    <>
+                                    <Loader2 className =" h-4 w-4 animate-spin  "/>
+                                    Creating...
+                                    </>
+                                ) : (
+                                    "Create Project"
+                                )
+
+                                }
+
+                        </Button>
+                    </DialogFooter>
                 </DialogContent>
             </Dialog>
         </>
